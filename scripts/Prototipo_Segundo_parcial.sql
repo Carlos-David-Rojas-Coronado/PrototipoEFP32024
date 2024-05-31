485081406ce60099d7803de2055cae7d55f5288f
Carlos-David-Rojas-Coronado <crojasc3@miumg.edu.gt>
2024-05-31 09:21:32 -0600

    creacion del proyectoy carpetas

---
+++ scripts/Prototipo_Segundo_parcial.sql
@@ -0,0 +1,21 @@
+DROP DATABASE dap;
+CREATE DATABASE dap;
+USE dap;
+CREATE TABLE IF NOT EXISTS usuario (
+  id_usuario INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(60) NOT NULL,
+  password VARCHAR(25)NOT NULL,
+  PRIMARY KEY (id_usuario) )
+ENGINE = InnoDB CHARACTER SET = latin1;
+-- -----------------------------------------------------
+-- Table `habitacion`.`habitacion`
+-- ---------------------------------------------usuario--------
+CREATE TABLE habitacion
+ (
+  id_habitacion VARCHAR(15),
+  id_tipoHabitacion VARCHAR(45),
+  id_tipocama VARCHAR(45),
+  precio VARCHAR(20),
+  estatus_habitacion VARCHAR(1),
+  PRIMARY KEY (id_habitacion)
+) ENGINE = InnoDB DEFAULT CHARSET=latin1;
